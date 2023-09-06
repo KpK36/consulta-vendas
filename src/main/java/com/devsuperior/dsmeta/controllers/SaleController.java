c7b8d46e07aa7a9969526a7b97982e83323e53bb
KpK36 <kayopatryck1@gmail.com>
2023-09-06 18:56:33 -0300

    Estruturação Controller

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,14 +1,15 @@
 package com.devsuperior.dsmeta.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.services.SaleService;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -24,9 +25,14 @@ public class SaleController {
 	}
 
 	@GetMapping(value = "/report")
-	public ResponseEntity<?> getReport() {
-		// TODO
-		return null;
+	public ResponseEntity<Page<SaleMinDTO>> getReport(@RequestParam String minDate,
+													  @RequestParam String maxDate,
+													  @RequestParam String name,
+													  Pageable pageable) {
+
+		return ResponseEntity.ok(service.salesSummaryPerSeller(minDate, maxDate, name, pageable));
+
+
 	}
 
 	@GetMapping(value = "/summary")
