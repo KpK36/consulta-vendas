eb3d67d49b08689a5ec847ba7922b03a7e3fdf67
KpK36 <kayopatryck1@gmail.com>
2023-09-06 18:56:53 -0300

    Estruturação Repository

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,23 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleMinDTO(obj.id, obj.amount, obj.date) "
+            + "FROM Sale obj "
+            + "WHERE obj.date BETWEEN :minDate AND :maxDate "
+            + "AND obj.seller.name LIKE CONCAT('%', :name, '%')")
+    Page<SaleMinDTO> sales(LocalDate minDate, LocalDate maxDate, String name, Pageable pageable);
 
 }
