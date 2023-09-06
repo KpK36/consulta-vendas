427102fc8d439a17265180353f23a73fd7a4e3a9
KpK36 <kayopatryck1@gmail.com>
2023-09-06 18:56:46 -0300

    Estruturação Service

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,14 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -12,12 +18,30 @@ import com.devsuperior.dsmeta.repositories.SaleRepository;
 @Service
 public class SaleService {
 
-	@Autowired
-	private SaleRepository repository;
-	
-	public SaleMinDTO findById(Long id) {
-		Optional<Sale> result = repository.findById(id);
-		Sale entity = result.get();
-		return new SaleMinDTO(entity);
-	}
+    @Autowired
+    private SaleRepository repository;
+
+    public SaleMinDTO findById(Long id) {
+        Optional<Sale> result = repository.findById(id);
+        Sale entity = result.get();
+        return new SaleMinDTO(entity);
+    }
+
+    public Page<SaleMinDTO> salesSummaryPerSeller(String minDate, String maxDate, String name, Pageable pageable) {
+
+        LocalDate minDateParse = LocalDate.parse(minDate);
+        LocalDate maxDateParse = LocalDate.parse(maxDate);
+
+        if (minDate.isBlank()) {
+            minDateParse = maxDateParse.minusYears(1L);
+
+        } else if (maxDate.isBlank()) {
+            maxDateParse = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        }
+
+        return repository.sales(minDateParse, maxDateParse, name, pageable);
+
+
+    }
 }
